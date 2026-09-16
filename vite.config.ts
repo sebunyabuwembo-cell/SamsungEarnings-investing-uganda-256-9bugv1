487da226b3a8b87638d511c9af34de4873b7fea8
onspace.ai <onspace.ai@gmail.com>
2026-09-16 11:06:15 +0300

    Fix corrupted vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/The-Engle-investment-/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+  base: '/SamsungEarnings-investing-uganda-256/',
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
