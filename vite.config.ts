f181058094972c9ca11624bacaaec9630698e7fd
sebunyabuwembo-cell <sebunyabuwembo@gmail.com>
2026-09-16 11:06:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,20 @@
-k
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/The-Engle-investment-/',
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
