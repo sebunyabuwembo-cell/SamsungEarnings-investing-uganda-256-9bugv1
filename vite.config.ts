4b2381e27e58433991ae8bb0856f42edfb067868
sebunyabuwembo-cell <sebunyabuwembo@gmail.com>
2026-09-16 12:07:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/SamsungEarnings-investing-uganda-256/',
+  base: '/The-Engle-investment-/',
   plugins: [react()],
   resolve: {
     alias: {
