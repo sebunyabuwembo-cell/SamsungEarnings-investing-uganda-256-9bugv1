e55d1b39e51cfe7469ab739e27a0985dd5cfa877
sebunyabuwembo-cell <sebunyabuwembo@gmail.com>
2026-09-18 23:37:12 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = 'https://iaodbzehficmwikzgtfs.supabase.co';
+const supabaseAnonKey = 'sb_publishable_CcjYhlZGo86Tyb3RDNo7DQ_Reojyco3';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
